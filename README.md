0f9c8982151ecd6739b10e9c5351ef18272ca34b
CathDalzell <catherine.j.dalzell@gmail.com>
2020-01-12 13:24:35 -0400

    Relaunch the repository for Java rather than Scala.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 StatisticalAlgorithms
+
+This project contains learning exercises in Java and basic algorithms from the Princeton algorithms course.
--- collections_ex1.scala
+++
@@ -1,7 +0,0 @@
-// Exercises about Scala collections
-
-/* We can get an array of type File and work with it */
-
-val a = new java.io.File(".").listFiles
-
-a.foreach(x => println(x.toString))
