74f82bb6840f835bb58734882f1c5817ee26f5f5
CathDalzell <catherine.j.dalzell@gmail.com>
2020-01-14 20:55:52 -0400

    Two simple for loops.

---
+++ JavaWork/fundamentals/src/part2/ForLoop.java
@@ -0,0 +1,22 @@
+package part2;
+
+/*
+ * A simple for loop, with increasing and decreasing counters
+ */
+public class ForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Increasing order");
+		for (int i = 0; i <= 9; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println("Decreasing order");
+		for (int k = 9; k >= 0; k--) {
+			System.out.println(k);
+		}
+
+	}
+
+}
